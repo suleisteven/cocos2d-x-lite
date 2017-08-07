3c0df2ebd964c68fef0d32a016e43e1ed0d11110
592672849@qq.com <592672849@qq.com>
2017-08-07 12:19:55 +0800

    modify android ssl,compile in so

--- external/sources/iflyteknet/android/Android.mk
+++ external/sources/iflyteknet/android/Android.mk
@@ -2,13 +2,21 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)  
 
+#添加预编译的openssl
+include $(CLEAR_VARS)
+LOCAL_MODULE := libopenssl
+LOCAL_SRC_FILES := $(LOCAL_PATH)/../../../android/$(TARGET_ARCH_ABI)/libssl.a
+include $(PREBUILT_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libcrypto
+LOCAL_SRC_FILES := $(LOCAL_PATH)/../../../android/$(TARGET_ARCH_ABI)/libcrypto.a
+include $(PREBUILT_STATIC_LIBRARY)
+
 LOCAL_MODULE := iflyteknet_shared
 LOCAL_MODULE_FILENAME := libiflyteknet
    
 LOCAL_SRC_FILES := 	../src/iflytek/net/TcpConnection.cpp \
-					../src/iflytek/net/RSAUtil.cpp	\
-					../src/iflytek/net/game.pb.cc	\
-					../src/iflytek/net/json_format.cc \
 					../src/iflytek/net/Compression.cpp
 					
 					
@@ -16,19 +24,23 @@ LOCAL_SRC_FILES := 	../src/iflytek/net/TcpConnection.cpp \
 LOCAL_C_INCLUDES := $(LOCAL_PATH)
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/../src/
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/../src/boost/
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../../../android/$(TARGET_ARCH_ABI)/include
 
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)
 LOCAL_EXPORT_C_INCLUDES += $(LOCAL_PATH)/../src/
 LOCAL_EXPORT_C_INCLUDES += $(LOCAL_PATH)/../src/boost/
 
-LOCAL_LDLIBS := -pthread -lz -L$(LOCAL_PATH)/prebuilt -lcrypto -lssl
+LOCAL_STATIC_LIBRARIES += libopenssl
+LOCAL_STATIC_LIBRARIES += libcrypto
+
+LOCAL_LDLIBS := -pthread -lz
 
 LOCAL_CFLAGS := -DASIO_STANDALONE
 
 #导入proto库
-LOCAL_WHOLE_STATIC_LIBRARIES += protobuf_static
+#LOCAL_WHOLE_STATIC_LIBRARIES += protobuf_static
 
 #include $(BUILD_STATIC_LIBRARY)
 include $(BUILD_SHARED_LIBRARY)
 
-$(call import-module,sources/protobuf/src)
+#$(call import-module,sources/protobuf/src)
